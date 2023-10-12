a7fe0738c3b12615c03c481e022a4590ce730c51
upendrari02 <upendrari02@gmail.com>
2023-10-13 02:59:31 +0530

    first commit

---
+++ src/main/java/com/upendra/api/GoogleLoginServiceApplication.java
@@ -0,0 +1,13 @@
+package com.upendra.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleLoginServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleLoginServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upendra/api/GoogleLoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upendra.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleLoginServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
